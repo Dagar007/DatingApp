4e20bd0c6cb32656b732b906b117161f716f4c1a
Deepak Kumar <dagardeepak88@gmail.com>
2025-07-13 14:11:36 +0530

    End of section 5

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,19 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { Nav } from "../layout/nav/nav";
+import { AccountService } from '../core/services/account-service';
+import { Home } from "../features/home/home";
+import { User } from '../types/user';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Nav, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App implements OnInit {
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected title = 'dating-app';
-  protected members = signal<any>([]);
+  protected members = signal<User[]>([]);
 
   ngOnInit() {
-    this.http.get('https://localhost:5001/api/members').subscribe({
+    this.http.get<User[]>('https://localhost:5001/api/members').subscribe({
       next: response => this.members.set(response),
       error: (error) => {
         console.error('Error fetching members:', error);
@@ -22,5 +27,13 @@ export class App implements OnInit {
         console.log('Member fetch complete');
       }
     });
+    this.setCurrentUser();
+  }
+
+  setCurrentUser() {
+    const user = localStorage.getItem('user');
+    if (user) {
+      this.accountService.currentUser.set(JSON.parse(user));
+    }
   }
 }
---
+++ client/src/core/services/account-service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { LoginCreds, RegisterCreds, User } from '../../types/user';
+import { tap } from 'rxjs/internal/operators/tap';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AccountService {
+  private http = inject(HttpClient);
+  currentUser = signal<User | null>(null);
+
+  baseUrl = 'https://localhost:5001/api/';
+
+  login(creds: LoginCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
+      tap((user) => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  register(registerCreds: RegisterCreds) {
+    return this.http
+      .post<User>(this.baseUrl + 'account/register', registerCreds)
+      .pipe(
+        tap((user) => {
+          if (user) {
+            this.setCurrentUser(user);
+          }
+        })
+      );
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUser.set(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+}
